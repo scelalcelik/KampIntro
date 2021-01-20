648eed66844990a8703f335482fdd228e238da75
scelalcelik <77546454+scelalcelik@users.noreply.github.com>
2021-01-20 23:51:24 +0300

    20 Ocak

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        /* void -> İşlem sonucunda ortaya çıkan sonuç başka bir yerde kullanılmak istenmiyorsa void kullanılır.
+         * Eğer çıkan sonuç başka bir yerde kullanılacaksa int kullanılmalıdır.
+    */
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " Eklendi.");
+        }
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " Güncellendi.");
+        }
+        public int Topla(int number1, int number2)
+        {
+            return number1 + number2;
+
+
+        }
+        public void Topla2(int number1, int number2)
+        {
+            Console.WriteLine(number1 + number2);
+        }
+
+        /* public void BiseyYap(int sayi)
+         {
+             sayi = 99;
+         }*/
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInsStock = 3;
+
+            Product product2 = new Product { Id = 2, CategoryId = 5, UnitsInsStock = 5,
+                ProductName = "Kalem", UnitPrice = 35 };
+
+            // PascalCase -- Classlarda Kelimelerin ilk harfi büyük olur
+            //camelCase -- Değişkenlerde ilk kelime küçük diğerleri büyük harfle başlar
+            //case sensitive -- Java C# gibi diller büyük küçük harf duyarlıdır.
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            // Console.WriteLine(product1.ProductName); ? -- Kamera -- Referans tip oldugundan
+
+            /*int sayi = 100;
+             productManager.BiseyYap(sayi);
+            // Console.WriteLine(sayi); ? -- 100 -- Deger tip oldugundan*/
+
+            //int double bool ... değer tip
+            //diziler,class,abstract,interface ... referans tip
+
+            productManager.Topla2(3, 6);
+            productManager.Topla(3, 6);
+        }
+    }
+}
